7acbf1e2ebe79408b8b4ac9dda07ea83988326bd
dandrea nicolas <dandrea.nicolas.ack@gmail.com>
2018-01-14 05:43:02 -0300

    [Added] default config. values.

--- src/Config/config.php
+++ src/Config/config.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
+	/**
+	 * Path where criteria will be stored.
+	 * By default, {root dir}/Criteria.
+	 */
 	'criteria_store_path' => 'Criteria'
 ];
--- src/Config/profiles.php
+++ src/Config/profiles.php
@@ -1,3 +1,12 @@
 <?php
 
-return [];
+return [
+	/**
+	 * 'exampleProfile' => [
+	 * 		\Name\Space\Of\Criteria\Criteria1::class,
+	 * 		\Name\Space\Of\Criteria\Criteria2::class,
+	 * 		\Name\Space\Of\Criteria\Criteria3::class,
+	 * 		\Name\Space\Of\Criteria\Criteria4::class,
+	 * ]
+	 */
+];
